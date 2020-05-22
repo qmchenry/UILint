3b184808a7d30d0a1780d621753411954f9130c0
qmchenry <quinn@jqm.us>
2020-05-22 13:56:27 -0400

    Rename library

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "QALint",
+    name: "UILint",
     platforms: [
         .iOS(.v12)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "QALint",
-            targets: ["QALint"]),
+            name: "UILint",
+            targets: ["UILint"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,10 +22,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "QALint",
+            name: "UILint",
             dependencies: []),
         .testTarget(
-            name: "QALintTests",
-            dependencies: ["QALint"]),
+            name: "UILintTests",
+            dependencies: ["UILint"]),
     ]
 )
