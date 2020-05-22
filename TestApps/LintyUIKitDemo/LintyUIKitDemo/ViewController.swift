a9781b8bb46b4e0cebf733096bfda68b8e370d3d
qmchenry <quinn@jqm.us>
2020-05-22 13:49:14 -0400

    Add basic UIKit app for testing

---
+++ TestApps/LintyUIKitDemo/LintyUIKitDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LintyUIKitDemo
+//
+//  Created by Quinn McHenry on 5/22/20.
+//  Copyright © 2020 Quinn McHenry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
