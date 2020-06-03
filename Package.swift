231a003e8180b62d7638d966488bc341ff436165
qmchenry <quinn@jqm.us>
2020-06-03 15:12:09 -0400

    Remove SimplePDF package dependency

--- Package.swift
+++ Package.swift
@@ -15,9 +15,7 @@ let package = Package(
             name: "UILint",
             targets: ["UILint"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/qmchenry/SimplePDF", .branch("master")),
-    ],
+    dependencies: [],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
